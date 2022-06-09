5c4ac80cf65213a6bc8c45f603a134836a625b8d
Süleyman Küskü <86887769+suleymankusku@users.noreply.github.com>
2022-06-09 14:00:48 +0300

    Add files via upload

---
+++ src/components/TodoForm.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+import 'react-calendar/dist/Calendar.css';
+
+function TodoForm(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const handleChange = e => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id:Math.random(),
+      text: input
+    });
+    setInput('');
+  };
+  return (
+    <form onSubmit={handleSubmit} className='todo-form'>
+      {props.edit ? (
+        <>
+          <input
+            placeholder='Güncelleyiniz'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            ref={inputRef}
+            className='todo-input edit'
+          />
+          <button onClick={handleSubmit} className='todo-button edit'>
+            Güncelleyiniz
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            placeholder='Ekle...'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputRef}
+          />
+          <button onClick={handleSubmit} className='todo-button'>
+            Ekle
+          </button>
+
+          <div className="filter-btns">
+            <select name="todos">
+                <option value="hepsi">Hepsi</option>
+                <option value="gunluk">Günlük</option>
+                <option value="haftalik">Hafttalık</option>
+                <option value="aylik"> Aylık</option>
+            </select>
+           </div>
+        </>
+      )}
+    </form>
+  );
+}
+
+export default TodoForm;
---
+++ src/components/TodoList.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = todo => {
+    if (todo.text==="") {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (newValue.text ==="" ) {
+      return;
+    }
+
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+  };
+
+  const removeTodo = id => {
+    const removedArr = [...todos].filter(todo => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <>
+      <h1>Yapılacaklar Listesi</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </>
+  );
+}
+
+export default TodoList;
